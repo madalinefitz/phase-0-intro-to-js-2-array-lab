af573c91ab29fe8e47858d8bd03084cd232e3852
madalinefitzpatrick <madaline.fitzpatrick@gmail.com>
2023-01-20 15:30:03 -0500

    commit tree 959df4161d1e369c08c085af2b9e3382874b919a

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    return cats.push(name)
+}
+function destructivelyPrependCat(name) {
+    return cats.unshift(name)
+}
+function destructivelyRemoveLastCat(name) {
+    return cats.pop(name)
+}
+function destructivelyRemoveFirstCat(name) {
+    return cats.shift(name)
+}
+function appendCat(name) {
+    return appendCat = [...cats, name]
+}
+function prependCat(name) {
+    return prependCat = [name, ...cats]
+}
+function removeLastCat(name) {
+    return removeLastCat  = cats.slice (0, cats.length -1)
+}
+function removeFirstCat(name) {
+    return removeFirstCat  = cats.slice (1)
+}
